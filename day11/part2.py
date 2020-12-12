b586880a124baa75dbd072fbe5f4ab8dec355dc1
Jonas Müller <jonas.mueller@compitencies.com>
2020-12-12 19:15:39 +0100

    Fix typo

--- day11/part2.py
+++ day11/part2.py
@@ -4,7 +4,7 @@ from common import SeatingSystem
 if __name__ == "__main__":
     # O(s*n^2) time and O(n) space
     # with s: number of steps until convergence, n: number of points in grid 
-    # Time complexity could be optimized by saving and reuisng neighbors of each point
+    # Time complexity could be optimized by saving and reusing neighbors of each point
     seating_system = SeatingSystem(5, False)
     seating_system.run()
     print(seating_system.occupied)
